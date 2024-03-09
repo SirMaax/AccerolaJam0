0d5279efd2f7e7e2929ba496031131377e4dd163
Dennis <dennis.schwartz.bielefeld@gmail.com>
2024-03-09 21:13:36 +0100

    start of managers

---
+++ Assets/Scripts/Systems/ProgressSystem.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressSystem : MonoBehaviour
+{
+    [Header("Progression")] 
+    public int currentSection;
+    public int currentSubSection;
+    
+    [Header("Requirements")]
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool CanPlayerProgress()
+    {
+        
+        return false;
+    }
+}
